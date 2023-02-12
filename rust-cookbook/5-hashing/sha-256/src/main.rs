efe159f020e232915f3c0a67d1524fa9a3914336
pea-sys <pea98258@gmail.com>
2023-02-12 15:21:46 +0900

    rust-cookbook 5~6

---
+++ rust-cookbook/5-hashing/sha-256/src/main.rs
@@ -0,0 +1,42 @@
+use data_encoding;
+use ring;
+
+use data_encoding::HEXUPPER;
+use ring::digest::{Context, Digest, SHA256};
+use std::fs::File;
+use std::io::{BufReader, Read, Write};
+use std::error::Error;
+
+fn sha256_digest<R: Read>(mut reader: R) -> Result<Digest, Box<dyn Error>> {
+    let mut context = Context::new(&SHA256);
+    let mut buffer = [0; 1024];
+
+    loop {
+        let count = reader.read(&mut buffer)?;
+        if count == 0 {
+            break;
+        }
+        context.update(&buffer[..count]);
+    }
+
+    Ok(context.finish())
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let path = "file.txt";
+
+    let mut output = File::create(path)?;
+    write!(output, "We will generate a digest of this text")?;
+
+    let input = File::open(path)?;
+    let reader = BufReader::new(input);
+    let digest = sha256_digest(reader)?;
+
+    println!("SHA-256 digest is {}", HEXUPPER.encode(digest.as_ref()));
+
+    Ok(())
+}
+
+fn main(){
+    run();
+}
