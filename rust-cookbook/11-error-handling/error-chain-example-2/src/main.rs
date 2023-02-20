90a0b38f5e06ec6291ec72e4ede22ec04ab2b8a8
pea-sys <pea98258@gmail.com>
2023-02-21 06:53:00 +0900

    rust-cookbook 11

---
+++ rust-cookbook/11-error-handling/error-chain-example-2/src/main.rs
@@ -0,0 +1,40 @@
+#[macro_use]
+extern crate error_chain;
+extern crate reqwest;
+
+use std::io::Read;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        Reqwest(reqwest::Error);
+        ParseIntError(std::num::ParseIntError);
+    }
+
+    errors { RandomResponseError(t: String) }
+}
+
+
+
+fn run() -> Result<()> {
+    let url =
+        format!("https://www.random.org/integers/?num=1&min=0&max=10&col=1&base=10&format=plain");
+    let response =reqwest::blocking::get(&url)?.text()?;
+    let random_value = response ;
+
+    println!("a random number between 0 and 10: {}", random_value);
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(error) = run() {
+        match *error.kind() {
+            ErrorKind::Io(_) => println!("Standard IO error: {:?}", error),
+            ErrorKind::Reqwest(_) => println!("Reqwest error: {:?}", error),
+            ErrorKind::ParseIntError(_) => println!("Standard parse int error: {:?}", error),
+            ErrorKind::RandomResponseError(_) => println!("User defined error: {:?}", error),
+            _ => println!("Other error: {:?}", error),
+        }
+    }
+}
